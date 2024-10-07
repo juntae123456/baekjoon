1f088a8988c343febb00bb92590d1125be54d4b4
JuntaeLee <esse3404028@naver.com>
2024-10-07 18:06:19 +0900

    2309번

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +10,7 @@ public class Main {
         for (int i = 0; i < a.length; i++) {
             a[i] = sc.nextInt();
         }
-        selection_sort(a);
+        Arrays.sort(a);
         for (int j : a) {
             sum += j;
         }
@@ -38,32 +39,4 @@ public class Main {
             }
         }
     }
-
-    public static void selection_sort(int[] a) {
-        selection_sort(a, a.length);
-    }
-
-    private static void selection_sort(int[] a, int size) {
-
-        for (int i = 0; i < size - 1; i++) {
-            int min_index = i;
-
-            // 최솟값을 갖고있는 인덱스 찾기
-            for (int j = i + 1; j < size; j++) {
-                if (a[j] < a[min_index]) {
-                    min_index = j;
-                }
-            }
-
-            // i번째 값과 찾은 최솟값을 서로 교환
-            swap(a, min_index, i);
-        }
-    }
-
-    private static void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-
 }
