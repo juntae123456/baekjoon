997c3f88b1f5ee048cca6fb7e8742f9122c8b4fa
mac <esse3404028@naver.com>
2024-11-19 14:52:20 +0900

    9086

---
+++ src/String.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i = 0; i < a; i++) {
+            java.lang.String b = sc.next();
+            for(int j = 0; j < b.length(); j++) {
+                if(b.charAt(j)=='0'){
+                    System.out.println("00");
+                    break;
+                }
+                if(j==0){
+                    System.out.printf("%c",b.charAt(j));
+                }
+                if(j+1==b.length()){
+                    System.out.printf("%c\n",b.charAt(j));
+                }
+            }
+        }
+    }
+}
