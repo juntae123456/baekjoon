ab3377c8f64ceb98329e349f3d99da6305453582
mac <esse3404028@naver.com>
2024-12-04 13:45:17 +0900

    24264

---
+++ src/Algorithm3.kt
@@ -0,0 +1,9 @@
+fun main(){
+    Algorithm3()
+}
+fun Algorithm3() {
+    val sc = java.util.Scanner(System.`in`)
+    val a = sc.nextLong()
+    println(Math.pow(a.toDouble(),2.0).toLong())
+    println("2")
+}
