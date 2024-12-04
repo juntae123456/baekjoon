4afe0002bd00c1b07584a37c62008ecd0e982496
mac <esse3404028@naver.com>
2024-12-04 14:01:14 +0900

    24264

--- src/Algorithm3.kt
+++ src/Algorithm3.kt
@@ -4,10 +4,6 @@ fun main(){
 fun Algorithm3() {
     val sc = java.util.Scanner(System.`in`)
     val a = sc.nextLong()
-    var b: Long = 0
-    for(i in 1 until  a){
-        b+=a-i
-    }
-    println(b)
+    println(Math.pow(a.toDouble(),2.0).toLong())
     println("2")
 }
