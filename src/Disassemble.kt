34add32fca544017b609aa581f86a95c68eb3a13
mac <esse3404028@naver.com>
2024-12-04 16:32:11 +0900

    2231

--- src/Disassemble.kt
+++ src/Disassemble.kt
@@ -5,13 +5,13 @@ fun Disassemble() {
     val sc = java.util.Scanner(System.`in`)
     val target = sc.nextInt()
 
-    val start = maxOf(1, target - target.toString().length * 9)
+    val start = maxOf(1, target - target.toString().length * 9) // 1부터 시작하거나, target - 9 * 자릿수 만큼 시작
     var constructor = 0
 
-    for (n in start until target) {
-        val digitSum = n.toString().map { it - '0' }.sum()
-        if (n + digitSum == target) {
-            constructor = n
+    for (i in start until target) {
+        val digitSum = i.toString().map { it - '0' }.sum() // 각 자리수의 합
+        if (i + digitSum == target) {
+            constructor = i
             break
         }
     }
